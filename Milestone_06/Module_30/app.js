f0c0413ba040bf7a6dd050603d0c22c31d0c3449
Erfan <deverfan44@gmail.com>
2025-03-06 14:14:15 +0600

    Explore Object seal, freeze, preventExtension

--- Milestone_06/Module_30/app.js
+++ Milestone_06/Module_30/app.js
@@ -162,17 +162,183 @@
 // console.log(newArr);
 
 
-let student = {
-    name: "Erfan",
-    age: 24,
-    isMale: true
-}
-let info = {
-    nationality: "Bangladeshi",
-    religion: "Islam"
-}
+// let student = {
+//     name: "Erfan",
+//     age: 24,
+//     isMale: true
+// }
+// let info = {
+//     nationality: "Bangladeshi",
+//     religion: "Islam"
+// }
 
-let newStu = {...student, ...info};
-console.log(newStu);
+// let newStu = {...student, ...info};
+// console.log(newStu);
 
 
+// Distructuring 
+// let student = {
+//     name: "Md Arfatul Islam",
+//     age: 24,
+//     nationality: "Bangladesh",
+//     isMale: true
+// }
+
+// const stdName = student.name;
+// const stdAge = student.age;
+// const stdNationality = student.nationality;
+// const stdIsMale = student.isMale;
+// console.log(stdName);
+// console.log(stdIsMale);
+
+// const {name, age, isMale, nationality} = student;
+// console.log(name);
+// console.log(nationality);
+// console.log(isMale);
+
+
+// Array Distructuring
+// const friends = ["Erfan","Jamil","Kamal",{info:"Different type of friends", number: 3}];
+// const [scientice, doctor, pilot, details] = friends;
+// console.log(scientice);
+// console.log(doctor);
+// console.log(pilot);
+// console.log(details);
+
+
+const vehicles = ['BMW','Suzuki','Mahindra Truck'];
+// const [car, motorbike, truck] = vehicles;
+
+// console.log(vehicles[0]);
+// console.log(vehicles[1]);
+// console.log(vehicles[2]);
+
+// console.log(car);
+// console.log(motorbike);
+// console.log(truck);
+
+// if we need only car and truck 
+// const [car, ,truck] = vehicles;
+// console.log(car);
+// console.log(truck);
+
+
+// UseCase of Distructuring 
+// function calculate(num1, num2) {
+//     const add = num1+num2;
+//     const mul = num1*num2;
+//     const sub = num1-num2;
+//     const division = num1/num2;
+
+//     return [add, mul, sub, division];
+// }
+
+// const result = calculate(10,5);
+// console.log(result);
+
+// const [addition, multiflication, subtraction, division] = calculate(10,5);
+// console.log(addition);
+// console.log(multiflication);
+// console.log(subtraction);
+// console.log(division);
+
+
+
+
+// function bmiCalculate({height,age}){
+//     return height*(age*age);
+// }
+
+// const student = {
+//     name: "Md Arfatul Islam",
+//     height: 5.7,
+//     age: 23,
+//     religion: "Islam"
+// }
+
+// const result = bmiCalculate(student);
+// console.log(result);
+
+
+// Object entries
+// let car = {
+//     model: "BMW",
+//     year: 2021,
+//     color: ['black','yellow','red']
+// }
+
+// console.log(Object.entries(car));
+// for(let arr of Object.entries(car)) {
+//     console.log(car[arr[0]]);
+// }
+
+
+// object seal
+
+// let student = {
+//     name: "Md Arfatul Islam",
+//     age: 24,
+// }
+// delete student.age;
+// console.log(student);
+
+// student.nationality = "Bangladeshi";
+// console.log(student);
+
+// console.log(Object.isSealed(student)); // it's checked object is seal or not
+
+// Object.seal(student); // Object is sealed in this line
+// student.color = 'Choklet'; // it's not add in object because this object is already seal
+// delete student.name; // it's not delete because this object is already seal
+// console.log(student);
+
+// console.log(Object.isSealed(student));
+
+// student.name = "tanim";
+// console.log(student); // After sealed modifications is allowed
+
+
+
+// object preventExtension
+// let student = {
+//     name: "Md Arfatul Islam",
+//     age: 24,
+// }
+// Object.preventExtensions(student);
+// student.name = "Karim";
+// console.log(student); // Afted preventExtension modifications is allowed
+
+// student.height = 5.7;
+// console.log(student); // After preventExtension addition is not allowed
+
+// delete student.age;
+// console.log(student); // After preventExtention deletion is allowed
+
+
+// Object freeze 
+// let student = {
+//     name: "Md Arfatul Islam",
+//     age: 24,
+// }
+// Object.freeze(student);
+// student.name = "Rahim"; // After freeze modifcation, addition, deletion not allowed
+// console.log(student);
+
+
+// Object seal in array 
+// const  fruits = ['orange', 'mango','banana'];
+// Object.seal(fruits);
+// fruits[1] = 'Jackfruits';
+// console.log(fruits); // After sealed modification is allowed but addition and deletion is not allowed
+
+// fruits.push("Gauva"); // it's show an error
+// console.log(fruits);
+
+
+// Object freeze in array 
+
+// const  fruits = ['orange', 'mango','banana'];
+// Object.freeze(fruits);
+
+// fruits[1] = "jackfruits"; // it's not work because array is already freeze
+// console.log(fruits);
